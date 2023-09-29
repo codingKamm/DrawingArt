aa92b02261b6c614a91733893fa16ad6c8b9ac62
Cameron Warner <cameron.w.warner@gmail.com>
2023-09-29 12:54:08 -0400

    Initial Commit

---
+++ DrawingArt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DrawingArt
+//
+//  Created by Cameron Warner on 9/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrawingArt/DrawingArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingArtApp.swift
+//  DrawingArt
+//
+//  Created by Cameron Warner on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
