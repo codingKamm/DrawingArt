400cd734cd38fb5335a72a3258b68859ddbb12ea
Cameron Warner <cameron.w.warner@gmail.com>
2023-10-16 15:42:15 -0400

    recommitting lost code from Xcode 15 update.

--- DrawingArt/ContentView.swift
+++ DrawingArt/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            Triangle()
+                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 300, height: 300)
+            
+            Circle()
+                       .stroke(.blue, lineWidth: 40)
+            VStack {
+                Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                    .stroke(.blue, lineWidth: 10)
+                    .frame(width: 300, height: 300)
+                
+                Path { path in
+                    path.move(to: CGPoint(x: 200, y: 100))
+                    path.addLine(to: CGPoint(x: 100, y: 300))
+                    path.addLine(to: CGPoint(x: 300, y: 300))
+                    path.addLine(to: CGPoint(x: 200, y: 100))
+                }
+                .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
