6c59e3dd9f806dabbbc0ae951a0df9d09577d805
Cameron Warner <cameron.w.warner@gmail.com>
2023-10-18 10:49:37 -0400

    practiced uisng frames, borders, and creating a background image border

---
+++ DrawingArt/BordersAndFillsViews.swift
@@ -0,0 +1,18 @@
+//
+//  BordersAndFillsViews.swift
+//  DrawingArt
+//
+//  Created by Cameron Warner on 10/18/23.
+//
+
+import SwiftUI
+
+struct BordersAndFillsViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BordersAndFillsViews()
+}
